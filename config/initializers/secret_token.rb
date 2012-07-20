ea8988908cb70faaa41480c8ed43c7ccba60908e
neocanable <neocanable@gmail.com>
2012-07-20 15:28:57 +0800

    init repo

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Shaidapei::Application.config.secret_token = 'eee9dac8da177792d70fa86eaa4b2287f2450c1cccab891df7512c708d6968d6fdbfa103534439d7f506fd7e9da714e4fead8a4e4ddb5eb4c7bb68ccfa98ff34'
